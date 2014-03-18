9d1a972927171f83e11375a47b83e116eff4384b
Levi Truelove <levi@levitruelove.com>
2014-03-17 21:34:13 -0400

    Got Jen's sprite almost working. There appears to be a bug with the onFloor() function in phaser.

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
 MainGame.Player = function(game, xPos, yPos, cursors) {
-    this.animFrameCount = 30;
+    this.animFrameCount = 20;
     this.speed = 250;
     this.atlasName = 'player';
     this.walkFrames = null;
@@ -19,12 +19,12 @@ MainGame.Player.prototype = Object.create(Phaser.Sprite.prototype);
 MainGame.Player.prototype.constructor = MainGame.Player;
 
 MainGame.Player.prototype.animatePlayer = function (){
-    this.walkFrames = Phaser.Animation.generateFrameNames('p3_walk', 1, 11, '', 2);
+    this.walkFrames = Phaser.Animation.generateFrameNames('jen_walk', 1, 12, '.png', 2);
     this.animations.add('walk', this.walkFrames, this.animFrameCount ,true,false);
 
-    this.animations.add('jump',["p3_jump"], this.animFrameCount , false, false);
-    this.animations.add('stand',["p3_stand"], this.animFrameCount , false, false);
-    this.animations.add('hurt',["p3_hurt"], this.animFrameCount , false, false);
+    this.animations.add('jump',["jen_jump.png"], this.animFrameCount , false, false);
+    this.animations.add('stand',["jen_walk01.png"], this.animFrameCount , false, false);
+    this.animations.add('hurt',["jen_hurt.png"], this.animFrameCount , false, false);
     
     // Set Anchor to the center of your sprite
     this.anchor.setTo(.5,1);
@@ -43,32 +43,32 @@ MainGame.Player.prototype.updatePlayer = function() {
             this.body.velocity.x = -1 * this.speed;
             // Invert scale.x to flip left/right
             this.scale.x = -1;
-            //this.animations.play('walk',this.animFrameCount,true);
+            this.animations.play('walk',this.animFrameCount,true);
         }
         // are we moving right?
         if (this.cursors.right.isDown){
             this.body.velocity.x = this.speed;
             this.scale.x = 1;
-            //this.animations.play('walk',this.animFrameCount,true);
+            this.animations.play('walk',this.animFrameCount,true);
         }
 
         //standing still
         if(this.body.velocity.x == 0){
-            //this.animations.stop('walk');
-            //this.animations.play('stand',this.animFrameCount);
+            this.animations.stop('walk');
+            this.animations.play('stand',this.animFrameCount);
         }
 
         if(this.body.onFloor()){
-            //this.animations.stop('jump');
+            this.animations.stop('jump');
             //did we press the jump key?
             if (this.cursors.up.isDown){
                 this.body.velocity.y = -2000;
-                //this.animations.stop('walk');
-                //this.animations.play('jump',this.animFrameCount);
+                this.animations.stop('walk');
+                this.animations.play('jump',this.animFrameCount);
             }
         }else{
-            //this.animations.stop('walk');
-            //this.animations.play('jump',this.animFrameCount);
+            this.animations.stop('walk');
+            this.animations.play('jump',this.animFrameCount);
         }
     }else{
         this.hurtCount--;
